c45ee72073a0e39656890764fdfc1a1df3739949
YashBorekar <yashborekar1@gmail.com>
2025-05-24 23:09:18 +0530

    handling duplicate case

--- Arrays/easy/unionArr.cpp
+++ Arrays/easy/unionArr.cpp
@@ -3,17 +3,28 @@ using namespace std;
 int main(){
     int A[] = {1,3,4,6,7,8};
     int sizeA = 5;
-    int B[] = {2,5,9,10};
+    int B[] = {2,5,3,9,10};
     int sizeB = 4;
 
     vector<int> result;
+
+    for(int i=0;i<sizeA;i++){
+    for(int j=0;j<sizeB;j++){
+        if(A[i]==B[j]){
+            B[j] = INT_MIN;
+            break;
+        }
+    }
+  }
 
     for(int i=0;i<sizeA;i++){
         result.push_back(A[i]);
     }
 
     for(int i=0;i<sizeB;i++){
-        result.push_back(B[i]);
+        if(B[i] != INT_MIN){
+            result.push_back(B[i]);
+        }
     }
     
     for(int i=0;i<result.size();i++){
